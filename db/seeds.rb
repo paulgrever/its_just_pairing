cc6ec55cd68f2f74fc9e67a547455e8ba0a020a2
Paul Grever <paulgrever@gmail.com>
2015-06-23 18:45:30 -0600

    finised iteration 1 and set up seed for 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,14 @@ Language.create(name: "PHP")
 Language.create(name: "Python")
 Language.create(name: "Perl")
 Language.create(name: "Clojure")
+
+User.create(login: "121watts",
+            uid: "7582765",
+            avatar_url: "https://avatars.githubusercontent.com/u/7582765?v=3",
+            html_url: "https://github.com/121watts",
+            description: "Want to pair on my .vimrc? I got tired of Dvorak so i've been devising my own keyboard layout from scratch")
+User.create(login: "BobGu",
+            uid: "4348166",
+            avatar_url: "https://avatars.githubusercontent.com/u/4348166?v=3",
+            html_url: "https://github.com/BobGu", 
+            description: "i am so alone plz pair with me")
